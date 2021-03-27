a537218fed98c1a53f767f6776363f652df1fac8
Otto Chang <schang96@gatech.edu>
2021-03-27 08:26:58 -0700

    2nd Ver.

--- 20210327Chapter4GuessNum.py
+++ 20210327Chapter4GuessNum.py
@@ -1,11 +1,16 @@
 import random
 r = random.randint(1,100)
+count = 0
+
 while True:
+	count += 1 #this is equal to count = count + 1
 	number = input('Please input your number:')
 	if int(number) == r:
 		print('You are right!')
+		print('This is your', count, 'time(s) trial')
 		break
 	elif int(number) > r:
 		print('Number is bigger than answer')
 	elif int(number) < r:
 		print('Number is smaller than answer')
+	print('This is your', count, 'time(s) trial')
